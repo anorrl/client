5d964339317b61aa557e3c4c724d60431d706666
grace <massive2bdamnedfan@gmail.com>
2026-03-26 06:03:23 +0000

    26, 03, 2026, 1

--- RobloxStudio/resource.h
+++ RobloxStudio/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by RobloxStudio_.rc
+// Used by RobloxStudio.rc
 //
 #define SW_HIDE                         0
 #define HIDE_WINDOW                     0
